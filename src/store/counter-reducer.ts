c81c24f0377588042e8d349be830241257f12cd3
borashek <borashek@inbox.ru>
2022-12-29 12:09:13 +0300

    no-reducer

---
+++ src/store/counter-reducer.ts
@@ -0,0 +1,23 @@
+export const MAX_VALUE_EVENTS = "MAX-VALUES-EVENTS" as const
+export const MIN_VALUE_EVENTS = "MIN-VALUES-EVENTS" as const
+
+import {ErrorType, SettingsType, DisabledType} from "../App";
+
+type MaxValuesEventsTypeAT = {
+  type: typeof MAX_VALUE_EVENTS
+  maxValue: number
+}
+type MinValuesEventsTypeAT = {
+  type: typeof MIN_VALUE_EVENTS
+  minValue: number
+}
+
+type ActionType = MaxValuesEventsTypeAT | MinValuesEventsTypeAT
+
+export const CounterReducer = (value: number | string, action: ActionType) => {
+  switch (action.type) {
+    case "MAX-VALUES-EVENTS":
+
+  }
+  return
+}
