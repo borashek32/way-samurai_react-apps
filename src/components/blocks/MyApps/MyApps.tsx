258d0d78f23e31095fb0fe78f7d32798028191fb
borashek <borashek@inbox.ru>
2023-01-09 19:45:40 +0300

    styles

--- src/components/blocks/MyApps/MyApps.tsx
+++ src/components/blocks/MyApps/MyApps.tsx
@@ -12,7 +12,7 @@ export const MyApps: React.FC<MyAppsType> = ({
     <div className={s.block}>
       <h1 className={s.blockTitle}>{name}</h1>
       <div className={s.blockFlex}>
-        <NavLink to={"/way-samurai_react-apps/telegram"} target={"_blank"}>
+        <NavLink to={"/way-samurai_react-apps/telegram"}>
           <div className={s.blockApp}>
             <h4 className={s.blockAppText}>Telegram clone</h4>
             <img src={require("./tl.jpg")} alt="telegram" className={s.blockSkillPicture}/>
@@ -21,7 +21,7 @@ export const MyApps: React.FC<MyAppsType> = ({
               maxime numquam quo repellendus soluta ullam voluptate.</p>
           </div>
         </NavLink>
-        <NavLink to={"https://borashek32.github.io/way-samurai_todolist-thursday/"} target={"_blank"}>
+        <NavLink to={"https://borashek32.github.io/way-samurai_todolist-thursday/"}>
           <div className={s.blockApp}>
             <h4 className={s.blockAppText}>Todolist</h4>
             <img src={require("./todolist.jpg")} alt="todolist" className={s.blockSkillPicture}/>
@@ -39,7 +39,7 @@ export const MyApps: React.FC<MyAppsType> = ({
               maxime numquam quo repellendus soluta ullam voluptate.</p>
           </div>
         </a>
-        <a href="https://borashek32.github.io/way-samurai_social-network_ts_old/" target={"_blank"}>
+        <a href="https://borashek32.github.io/way-samurai_social-network_ts_old/">
           <div className={s.blockApp}>
             <h4 className={s.blockAppText}>Social Network</h4>
             <img src={require("./social-network.jpg")} alt="social-network" className={s.blockSkillPicture}/>
